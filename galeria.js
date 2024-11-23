af23176096a84dce2c2f013a2a8ec0fc22fa58bb
lavisuelen9999999 <laviviasuelem13@gmail.com>
2024-11-22 21:35:35 -0300

    tcc

--- galeria.js
+++ galeria.js
@@ -2,7 +2,7 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
        menuMobile.classList.remove('open');
-       document.getElementById('.titulo').style.marginTop = "0"; // Move o título de volta para o topo
+       document.getElementById('.titulo').style.marginTop = "150px"; // Move o título de volta para o topo
     } else {
        menuMobile.classList.add('open');
        document.getElementById('.titulo').style.marginTop = "150px"; // Move o título para baixo do cabeçalho
