0ee28dd4929e2dc2909c390e59ca30e7f4f5d658
lavisuelen9999999 <laviviasuelem13@gmail.com>
2024-11-22 21:23:26 -0300

    tcc

--- origem.js
+++ origem.js
@@ -3,9 +3,9 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
        menuMobile.classList.remove('open');
-       document.getElementById('sumario').style.marginTop = "150px"; // Move o título de volta para o topo
+       document.getElementById('sumario').style.marginTop = "0"; // Move o título de volta para o topo
     } else {
        menuMobile.classList.add('open');
-       document.getElementById('sumario').style.marginTop = "500px"; // Move o título para baixo do cabeçalho
+       document.getElementById('sumario').style.marginTop = "0"; // Move o título para baixo do cabeçalho
     }
  }
--- religião.js
+++ religião.js
@@ -2,9 +2,9 @@ function menuShow(){
    let menuMobile = document.querySelector('.mobile-menu');
    if (menuMobile.classList.contains('open')){
       menuMobile.classList.remove('open');
-      document.getElementById('titulo').style.marginTop = "0px"; // Move o título de volta para o topo
+      document.getElementById('#sumario').style.marginTop = "0px"; // Move o título de volta para o topo
    } else {
       menuMobile.classList.add('open');
-      document.getElementById('titulo').style.marginTop = "200px"; // Move o título para baixo do cabeçalho
+      document.getElementById('#sumario').style.marginTop = "200px"; // Move o título para baixo do cabeçalho
    }
 }
