e5caa35c898e6b86d64fbf9fe406a3c1d8da2796
lavisuelen9999999 <laviviasuelem13@gmail.com>
2024-11-22 22:12:56 -0300

    tcc

--- origem.js
+++ origem.js
@@ -1,11 +1,11 @@
 
 function menuShow(){
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')){
-       menuMobile.classList.remove('open');
-       document.getElementById('sumario').style.marginTop = "0"; // Move o título de volta para o topo
-    } else {
-       menuMobile.classList.add('open');
-       document.getElementById('sumario').style.marginTop = "0"; // Move o título para baixo do cabeçalho
-    }
- }
+   let menuMobile = document.querySelector('.mobile-menu');
+   if (menuMobile.classList.contains('open')){
+      menuMobile.classList.remove('open');
+      document.getElementById('sumario').style.marginTop = "0"; // Move o título de volta para o topo
+   } else {
+      menuMobile.classList.add('open');
+      document.getElementById('sumario').style.marginTop = "250px"; // Move o título para baixo do cabeçalho
+   }
+}
